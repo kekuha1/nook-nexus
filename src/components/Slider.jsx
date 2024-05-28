07aa2eac4688bfc5a7f7d455b3fd48713399bb13
kekuha1 <109771410+kekuha1@users.noreply.github.com>
2024-05-28 10:53:42 -0400

    swiper imports showing up as not used

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,11 +2,11 @@ import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { collection, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { db } from "../firebase.config";
-import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper-bundle.css";
 import Spinner from "./Spinner";
-SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+
 
 function Slider() {
   const [loading, setLoading] = useState(true);
